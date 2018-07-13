5a235062af318bd089f7580084368d12add94bc7
fgolf <frank.golf@gmail.com>
2018-07-12 22:37:47 -0500

    make general for arbtirary number of particles

--- reader.py
+++ reader.py
@@ -6,26 +6,38 @@ def convert_lhe(fname_in, fname_out="events.root"):
 
     f1 = r.TFile(fname_out, "recreate")
     t1 = r.TTree("t","t")
-    pdgid = array( 'i', [ 0,0,0,0,0 ] )
-    status = array( 'i', [ 0,0,0,0,0 ] )
-    parent1 = array( 'i', [ 0,0,0,0,0 ] )
-    parent2 = array( 'i', [ 0,0,0,0,0 ] )
-    color1 = array( 'i', [ 0,0,0,0,0 ] )
-    color2 = array( 'i', [ 0,0,0,0,0 ] )
-    mass = array( 'd', [ 0,0,0,0,0 ] )
-    spin = array( 'd', [ 0,0,0,0,0 ] )
+
+    pdgid = r.std.vector(int)()
+    status = r.std.vector(int)()
+    parent1 = r.std.vector(int)()
+    parent2 = r.std.vector(int)()
+    color1 = r.std.vector(int)()
+    color2 = r.std.vector(int)()
+    mass = r.std.vector(float)()
+    spin = r.std.vector(float)()
     p4s = r.std.vector(r.TLorentzVector)()
-    t1._p4 = p4s
+
+    t1._pdgid   = pdgid    
+    t1._status  = status 
+    t1._parent1 = parent1
+    t1._parent2 = parent2
+    t1._color1  = color1 
+    t1._color2  = color2 
+    t1._mass    = mass  
+    t1._spin    = spin  
+    t1._p4      = p4s  
+
     p4 = r.TLorentzVector(1,1,0,5)
-    t1.Branch("id",pdgid, "id[5]/I")
-    t1.Branch("status",status, "status[5]/I")
-    t1.Branch("parent1",parent1, "parent1[5]/I")
-    t1.Branch("parent2",parent2, "parent2[5]/I")
-    t1.Branch("color1",color1, "color1[5]/I")
-    t1.Branch("color2",color2, "color2[5]/I")
-    t1.Branch("mass",mass, "mass[5]/F")
-    t1.Branch("spin",spin, "spin[5]/F")
-    t1.Branch("p4",p4s)
+
+    t1.Branch("id", pdgid)
+    t1.Branch("status", status)
+    t1.Branch("parent1", parent1)
+    t1.Branch("parent2", parent2)
+    t1.Branch("color1", color1)
+    t1.Branch("color2", color2)
+    t1.Branch("mass", mass)
+    t1.Branch("spin", spin)
+    t1.Branch("p4", p4s)
 
     event = ""
     in_event = False
@@ -37,7 +49,17 @@ def convert_lhe(fname_in, fname_out="events.root"):
                 in_event = False
 
                 particle_lines = event.splitlines()[1:]
+
+                pdgid.clear()
+                status.clear()
+                parent1.clear()
+                parent2.clear()
+                color1.clear()
+                color2.clear()
+                mass.clear()
+                spin.clear()
                 p4s.clear()
+    
                 for index,particle_line in enumerate(particle_lines):
                     parts = particle_line.split()
                     evt_pdgid = int(parts[0])
@@ -49,15 +71,17 @@ def convert_lhe(fname_in, fname_out="events.root"):
                     evt_px, evt_py, evt_pz, evt_e = map(float,parts[6:10])
                     evt_mass = float(parts[10])
                     evt_spin = float(parts[11])
+
                     p4.SetPxPyPzE(evt_px, evt_py, evt_pz, evt_e)
-                    pdgid[index] = evt_pdgid
-                    status[index] = evt_status
-                    parent1[index] = evt_parent1
-                    parent2[index] = evt_parent2
-                    color1[index] = evt_color1
-                    color2[index] = evt_color2
-                    mass[index] = evt_mass
-                    spin[index] = evt_spin
+
+                    pdgid.push_back(evt_pdgid)
+                    status.push_back(evt_status)
+                    parent1.push_back(evt_parent1)
+                    parent2.push_back(evt_parent2)
+                    color1.push_back(evt_color1)
+                    color2.push_back(evt_color2)
+                    mass.push_back(evt_mass)
+                    spin.push_back(evt_spin)
                     p4s.push_back(p4)
 
                 t1.Fill()
